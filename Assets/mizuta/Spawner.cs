925f4446f90ad7d9d948dfc2f41d0e3d1beb53c0
KND9908 <jikumaru954@gmail.com>
2022-08-10 18:03:45 +0900

    スポナー作成 ※Capsuleプレハブはテストで作ったもののため無視してOK

---
+++ Assets/mizuta/Spawner.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private float _interval = 3; // 生成間隔
+    [SerializeField]
+    private float spawn_x,spawn_y,spawn_z;
+    [SerializeField]
+    private int spawnmax = 3;//合計スポーン可能数
+    [SerializeField]
+    private GameObject _original = null; // スポーン時に複製するゲームオブジェクト
+
+    private GameObject enemy = null;
+
+    private float _elapsed; // 経過時間
+    void Update()
+    {
+        _elapsed += Time.deltaTime;
+        if (this.transform.childCount < spawnmax)//子オブジェクトの数
+        {
+            if (_elapsed > _interval)
+            {
+                _elapsed = 0;
+                enemy = Instantiate(_original);
+                enemy.transform.position = new Vector3(spawn_x,spawn_y,spawn_z);
+                //複製オブジェクトはスポナーの子オブジェクトに
+                enemy.transform.parent = this.transform;
+                var rigidbody = enemy.GetComponent<Rigidbody>();
+            }
+        }
+    }
+}
