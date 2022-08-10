2ff31a06c2a4bd1ef5b50e22bd35d0f6cb521e9e
sinzitoyama <yamat422@gmail.com>
2022-08-10 18:06:16 +0900

    エネミームーブ

--- Assets/Toyama/EnemyMove.cs
+++ Assets/Toyama/EnemyMove.cs
@@ -7,14 +7,17 @@ public class EnemyMove : MonoBehaviour
     [SerializeField] GameObject _player1 = default;
     [SerializeField] float _speed = 3f;
 
+    public Player _check;
+
     void Start()
     {
-
+        _check = FindObjectOfType<Player>();
     }
     private void OnTriggerStay(Collider collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && _check._enemyCheck == false)
         {
+            Debug.Log("ugoiteru");
             Vector3 targeting = (_player1.transform.position - transform.position).normalized;//プレイヤー-敵キャラの位置関係から方向を取得し、速度を一定化
             GetComponent<Rigidbody>().velocity = new Vector3(targeting.x * _speed, 0);//プレイヤー追う
             if (targeting.x > 0)
