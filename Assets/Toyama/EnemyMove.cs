7d1fc71a60ea462185a3a764fde12aceb55df3d1
sinzitoyama <yamat422@gmail.com>
2022-08-10 14:00:26 +0900

    エネミームーブ完成

---
+++ Assets/Toyama/EnemyMove.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] GameObject _player1 = default;
+    [SerializeField] float _speed = 3f;
+
+    void Start()
+    {
+
+    }
+    private void OnTriggerStay(Collider collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Vector3 targeting = (_player1.transform.position - transform.position).normalized;//プレイヤー-敵キャラの位置関係から方向を取得し、速度を一定化
+            GetComponent<Rigidbody>().velocity = new Vector3(targeting.x * _speed, 0);//プレイヤー追う
+            if (targeting.x > 0)
+            {
+                transform.rotation = new Quaternion(0, 1f, 0, 0);
+            }
+            else
+            {
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+            }
+        }
+    }
+}
