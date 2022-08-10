e5bbb77046e9b0dd1b7bb967de73f599e3746e1a
Oenz <oenz925@gmail.com>
2022-08-10 16:13:15 +0900

    プレイヤー向きz軸からx軸基準に

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -12,12 +12,27 @@ public class Player : MonoBehaviour
     Rigidbody _rb;
     CapsuleCollider _capsule;
     float _horizontal;
-
+    Ability _ability;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _capsule = GetComponent<CapsuleCollider>();
+        _ability = GetComponent<Ability>();
+
+        var _health = GetComponent<Health>();
+        _health.OnHealthChanged += HealthChange;
+        _health.OnDeath += Death;
+    }
+
+    void HealthChange(float hp)
+    {
+
+    }
+
+    void Death()
+    {
+
     }
 
     void Update()
@@ -48,14 +63,17 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        _rb.AddForce(_rb.transform.up * _jumpPower, ForceMode.Impulse);
+        _rb.AddForce(Vector3.up * _jumpPower, ForceMode.Impulse);
     }
     
     private void FixedUpdate()
     {
-        Vector3 dir = gameObject.transform.forward * _horizontal * _speed;
+        Vector3 dir = Vector3.right * _horizontal * _speed;
         dir.y = _rb.velocity.y;
         if (dir.x > _maxSpeed) dir = new Vector3(_maxSpeed, dir.y, dir.z);
+        if (dir.x < -_maxSpeed) dir = new Vector3(-_maxSpeed, dir.y, dir.z);
+        if (dir.z > _maxSpeed) dir = new Vector3(dir.x, dir.y, _maxSpeed);
+        if (dir.z < -_maxSpeed) dir = new Vector3(dir.x, dir.y, -_maxSpeed);
         _rb.velocity = dir;
     }
 
@@ -64,10 +82,12 @@ public class Player : MonoBehaviour
         Vector3 pos = Input.mousePosition;
         pos.z = 10.0f;
         Vector3 t = Camera.main.ScreenToWorldPoint(pos);
-        t.x = 0;
+        t.z = 0;
 
-        Vector3 s = transform.position + transform.forward * 2;
-        s.x = 0;
+        if (_ability.ThrowObject(t)) return;
+
+        Vector3 s = transform.position;// + transform.forward * 2;
+        s.z = 0;
        // Debug.DrawLine(t, s, Color.red, 2f);
         Vector3 dir =  t - s;
         dir *= 1000;
@@ -76,6 +96,10 @@ public class Player : MonoBehaviour
         if (Physics.Raycast(s, dir, out hit))
         {
             hit.collider.gameObject.GetComponent<IInteract>()?.Interact();
+            if (hit.collider.gameObject.CompareTag("Enemy") || hit.collider.gameObject.CompareTag("Object"))
+            {
+                _ability.EquipObject(hit.collider.gameObject);
+            }
             Debug.Log(hit.transform.position);
             Debug.DrawLine(hit.transform.position, hit.transform.position + Vector3.up, Color.green, 5);
         }
